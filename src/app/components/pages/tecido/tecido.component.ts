e6e0294bfb67b0e8b00f284bfd6b81b9678d7ff2
TI 03 <ti03@dc.net>
2023-05-10 10:30:23 -0300

    layout tecnologia

--- src/app/components/pages/tecido/tecido.component.ts
+++ src/app/components/pages/tecido/tecido.component.ts
@@ -23,7 +23,7 @@ export class TecidoComponent implements OnInit{
   modalRef?: BsModalRef;
   fornecedor?: FornecedorInterface;
   favoritar:string = "";
-
+  tecnologias:string = '1' + this.tecido?.dry;
 
   constructor(
     private tecidoService: TecidoService, 
@@ -33,7 +33,9 @@ export class TecidoComponent implements OnInit{
     private fornecedorService: FornecedorService
     ){}
 
-
+    get tecnologiasi() {
+      return this.tecnologias;
+    }
 
   ngOnInit(): void {
     const id = Number(this.route.snapshot.paramMap.get('id'));
@@ -67,4 +69,95 @@ export class TecidoComponent implements OnInit{
       this.fornecedorService.getFornecedor(id).subscribe((item) => (this.fornecedor = item.data));
     }
   }
+
+  getTecnologias(): string {
+    let result = '';
+    if (this.tecido?.uv) {
+      result += 'UV - ';
+    }
+    if (this.tecido?.dry) {
+      result += 'DRY - ';
+    }
+    if (this.tecido?.insect) {
+      result += 'INSECT - ';
+    }
+    if (this.tecido?.smart) {
+      result += 'SMART - ';
+    }
+    if (this.tecido?.defense) {
+      result += 'DEFENSE - ';
+    }
+    if (this.tecido?.chlomax) {
+      result += 'CHLOMAX - ';
+    }
+    if (this.tecido?.hydro) {
+      result += 'HYDRO - ';
+    }
+    if (this.tecido?.eco) {
+      result += 'ECO - ';
+    }
+    if (this.tecido?.shield) {
+      result += 'SHIELD - ';
+    }
+    if (this.tecido?.undertech) {
+      result += 'UNDERTECH - ';
+    }
+    if (this.tecido?.ultraflex) {
+      result += 'ULTRAFLEX - ';
+    }
+    if (this.tecido?.durabilidade) {
+      result += 'DURABILIDADE - ';
+    }
+    if (this.tecido?.toqueaveludado) {
+      result += 'TOQUE AVELUDADO - ';
+    }
+    if (this.tecido?.respirabilidade) {
+      result += 'RESPIRABILIDADE - ';
+    }
+    if (this.tecido?.duplaface) {
+      result += 'DUPLA FACE - ';
+    }
+    if (this.tecido?.leveza) {
+      result += 'LEVEZA - ';
+    }
+    if (this.tecido?.altacobertura) {
+      result += 'ALTA COBERTURA - ';
+    }
+    if (this.tecido?.elasticidade) {
+      result += 'ELASTICIDADE - ';
+      }
+      if (this.tecido?.secagemrapida) {
+      result += 'SECAGEM RÁPIDA - ';
+      }
+      if (this.tecido?.toquegelado) {
+      result += 'TOQUE GELADO - ';
+      }
+      if (this.tecido?.toquemacio) {
+      result += 'TOQUE MACIO - ';
+      }
+      if (this.tecido?.toquedebrilho) {
+      result += 'TOQUE DE BRILHO - ';
+      }
+      if (this.tecido?.zerotransparencia) {
+      result += 'ZERO TRANSPARÊNCIA - ';
+      }
+      if (this.tecido?.naoesgarca) {
+      result += 'NÃO ESGARÇA - ';
+      }
+      if (this.tecido?.naopinica) {
+      result += 'NÃO PINICA - ';
+      }
+      if (this.tecido?.oekotex) {
+      result += 'OEKO-TEX - ';
+      }
+      if (this.tecido?.compressao) {
+      result += 'COMPRESSÃO FIRME - ';
+      }
+      if (this.tecido?.controledeodor) {
+      result += 'CONTROLE DE ODOR - ';
+      }
+
+      return result
+    }
+
 }
